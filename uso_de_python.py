75943b71dd55c2c86eb462237397ac1d72f443ac
germanmarquez56 <germanmarquez17@gmai.com>
2025-03-14 19:18:22 -0500

    introduccion a python

---
+++ uso_de_python.py
@@ -0,0 +1,30 @@
+# Autor: German Marquez
+# Este es un comentario de una sola linea
+
+print("estudiando python")
+
+#Definicion de variables
+'''
+reglas:
+pyton es sencible a minusculas y mayusculas
+No se puede usar espacios
+no es conveniente usar numeros como nombre de variables
+'''
+x=50
+y=15
+
+print(x+y)
+
+print(type(x))
+#<class 'int'> : significa que x es un numero entero
+nombre="German"
+print(nombre)
+print(type(nombre))
+#<class 'str'> : significa que la variable nombre es un string (cadena)
+
+print(type(x))
+x=4.8
+print(type(x))
+#<class 'float'> : significa que la variable es un flotante (numero decimal)
+
+print(x , nombre)
